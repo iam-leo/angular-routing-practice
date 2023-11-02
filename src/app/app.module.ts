15988d8ae8a5fac5c6bba874e2c7c7906fdbd40c
Javier Leonel Sosa <leo.vm.cba@gmail.com>
2023-11-02 20:31:26 -0300

    add: general components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { FooterComponent } from './components/dashboard/footer/footer.component';
+import { NavbarComponent } from './components/dashboard/navbar/navbar.component';
+import { ListUsersComponent } from './components/dashboard/list-users/list-users.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    FooterComponent,
+    NavbarComponent,
+    ListUsersComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
