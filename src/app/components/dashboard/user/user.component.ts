b1f3625c77e675f7884dcc0eab7ae36d9f357f28
Javier Leonel Sosa <leo.vm.cba@gmail.com>
2023-11-06 20:48:40 -0300

    add: card for single user

--- src/app/components/dashboard/user/user.component.ts
+++ src/app/components/dashboard/user/user.component.ts
@@ -9,18 +9,26 @@ import { UsersService } from 'src/app/services/users.service';
 })
 export class UserComponent implements OnInit {
   id: number;
+  name: string = '';
+  email: string = '';
+  gender: string = '';
+  status: string = '';
+  imageUrl = 'https://pixabay.com/get/g4988ac51802c4b8d0a3877f1d621898e369d66f8ba20a353240e6ca4ea6489a1b124b2dd7d9ee70249238e42e88ea710_1280.png';
 
   constructor( private actRoute: ActivatedRoute, private _usersService: UsersService) {
     this.id = +this.actRoute.snapshot.params['id'];
    }
 
    ngOnInit(): void {
-     this.getUser(this.id)
+     this.getUser(this.id);
    }
 
    getUser(id: number){
     this._usersService.getUserDetails(id).subscribe(data => {
-      console.log(data)
+      this.name = data.name;
+      this.email = data.email;
+      this.gender = data.gender;
+      this.status = data.status;
     })
    }
 
